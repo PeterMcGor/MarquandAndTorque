aca5f461178f70bde31a70cdd2947300996af6b3
PeterMcGor <pedro.macias.gordaliza@gmail.com>
2019-10-08 11:12:22 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,15 @@
-FROM ubuntu:16.04
+FROM intelpython/intelpython3_full
+
 #FROM ubuntu:18.04
-
-
-RUN apt-get update &&\
-            apt-get update -y &&\
-            apt-get install build-essential -y &&\
-            apt-get install git -y &&\
-            apt-get install libtool m4 automake pkg-config -y &&\
-            apt-get install libssl-dev libxml2-dev zlib1g-dev libboost-dev pbs-drmaa-dev gperf -y
-
-RUN apt-get install environment-modules wget -y
-
-RUN wget --no-check-certificate https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda && \
-    rm Miniconda3-latest-Linux-x86_64.sh
-ENV PATH /opt/conda/bin:${PATH}
 ENV LANG C.UTF-8
-
 WORKDIR /
 COPY setup.py setup.py
 RUN pip install -e .
 COPY nispat/ nispat
 WORKDIR /nispat
-COPY test_normative_modeling/ test_normative_modeling
-WORKDIR /
-RUN chmod -R 777 /nispat/*
-#ENTRYPOINT /bin/bash
-ENTRYPOINT /opt/conda/bin/python -u /nispat/nispat/normative.py -c /mnt/data/test_normative_modeling/covariates_HC.txt -t /mnt/data/test_normative_modeling/covariates_allpatients.txt -r /mnt/data/test_normative_modeling/features_allpatients.txt -a gpr  /mnt//data/test_normative_modeling/features_HC.txt
+#COPY test_normative_modeling/ test_normative_modeling
+#WORKDIR /
+#RUN chmod -R 777 /nispat/*
+ENTRYPOINT /bin/bash
+#ENTRYPOINT /opt/conda/bin/python -u /nispat/nispat/normative.py -c /mnt/data/test_normative_modeling/covariates_HC.txt -t /mnt/data/test_normative_modeling/covariates_allpatients.txt -r /mnt/data/test_normative_modeling/features_allpatients.txt -a gpr  /mnt//data/test_normative_modeling/features_HC.txt
 
--- nispat/nispat/utils_misc.py
+++ nispat/nispat/utils_misc.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 import glob
-import yaml
-from nispat.nispat.normative_parallel import split_nm
+from nispat.nispat.normative_parallel import split_nm, collect_nm
+from nispat.nispat import normative
 
 
 def generate_fake_data(output_path, n_features, n_subjects, n_patients=0.1):
@@ -11,46 +11,54 @@ def generate_fake_data(output_path, n_features, n_subjects, n_patients=0.1):
     features = np.random.random((n_subjects, n_features))
     np.savetxt(os.path.join(output_path, "covariates_allpatients.txt"), covariates, fmt='%i')
     np.savetxt(os.path.join(output_path, "features_allpatients.txt"), features, fmt="%f")
-    np.savetxt(os.path.join(output_path, "covariates_HC.txt"), covariates[:int(n_subjects*(1 - n_patients))], fmt='%i')
+    np.savetxt(os.path.join(output_path, "covariates_HC.txt"), covariates[:int(n_subjects * (1 - n_patients))],
+               fmt='%i')
     np.savetxt(os.path.join(output_path, "features_HC.txt"), features[:int(n_subjects * (1 - n_patients))], fmt='%f')
 
 
-def modify_yml_file(yml_file, dict_params_to_modify, outfile=None):
-    with open(yml_file, 'r') as stream:
-        params = yaml.load(stream)
-    params = {**params, **dict_params_to_modify}
-    outfile = yml_file if outfile is None else outfile
-    with open(outfile, 'w') as writer:
-        yaml.dump(params, writer, default_flow_style=False)
-
-def generate_k8s_kubernetes(processing_dir, respfile, covfile, yml_template, maskfile=None, cvfolds=None, testcov=None, testresp=None,
-                            alg='gpr', configparam=None, saveoutput=True, outputsuffix=None, features_per_batch=50,
-                            binary=False):
+def generate_batches(processing_dir, respfile, testresp=None, features_per_batch=50, binary=False):
     def create_path(file):
         return os.path.join(processing_dir, file) if file is not None else None
 
     def create_paths(file_list):
         return [create_path(file) for file in file_list]
 
-    respfile, covfile, maskfile,testcov, testresp = create_paths([respfile, covfile, maskfile, testcov, testresp])
+    respfile, testresp = create_paths([respfile, testresp])
     split_nm(processing_dir, respfile, features_per_batch, binary, testresp)
-    batch_dirs = glob.glob(os.path.join(processing_dir, 'batch_*'))
+
+
+def fit_per_batches(processing_dir, covfile, testcov=None, maskfile=None, from_to=None, batch_pattern='batch_',
+                    alg='gpr', cvfolds=None, configparam=None, saveoutput=True, outputsuffix=None):
+    batch_dirs = glob.glob(os.path.join(processing_dir, batch_pattern + '*')) if from_to is None else \
+        [os.path.join(processing_dir, batch_pattern + str(batch_number)) for batch_number in
+         range(from_to[0], from_to[1])]
+    print(batch_dirs)
+    init_dir = os.getcwd()
     for batch_dir in batch_dirs:
         batch_respfile = glob.glob(os.path.join(batch_dir, "resp_batch_*"))
         batch_respfile = None if len(batch_respfile) != 1 else batch_respfile[0]
+        # These two must be always present
+        assert batch_respfile is not None
+        assert covfile is not None
         batch_testrespfile = glob.glob(os.path.join(batch_dir, "testresp_batch_*"))
         batch_testrespfile = None if len(batch_testrespfile) != 1 else batch_testrespfile[0]
         print(batch_respfile)
         print(batch_testrespfile)
-
-
-
+        os.chdir(batch_dir)
+        normative.estimate(batch_respfile, covfile, maskfile=maskfile, cvfolds=cvfolds, testcov=testcov,
+                           testresp=batch_testrespfile, alg=alg, configparam=configparam, saveoutput=saveoutput,
+                           outputsuffix=outputsuffix)
+    os.chdir(init_dir)
 
 
 
 if __name__ == '__main__':
     print("Utils_misc")
-    #generate_fake_data('/home/pmacias/Projects/JanJo/MarquandAndTorque/fake_data_3', 50, 700)
-    generate_k8s_kubernetes("/home/pmacias/Projects/JanJo/MarquandAndTorque/FakeData/fake_data/", "features_HC.txt",
-                            "covariates_HC.txt", testcov="covariates_allpatients.txt",
-                            testresp="features_allpatients.txt")
+    # generate_fake_data('/home/pmacias/Projects/JanJo/MarquandAndTorque/fake_data_3', 50, 700)
+    # generate_batches("/home/pmacias/Projects/JanJo/MarquandAndTorque/test_normative_modeling/", "features_HC.txt",
+    #                  testresp="features_allpatients.txt", features_per_batch=5)
+    # fit_per_batches("/home/pmacias/Projects/JanJo/MarquandAndTorque/test_normative_modeling/",
+    #                 "/home/pmacias/Projects/JanJo/MarquandAndTorque/test_normative_modeling/covariates_HC.txt",
+    #                 testcov="/home/pmacias/Projects/JanJo/MarquandAndTorque/test_normative_modeling/covariates_allpatients.txt",
+    #                 from_to=[1, 11])
+    # collect_nm("/home/pmacias/Projects/JanJo/MarquandAndTorque/test_normative_modeling/", collect=True)
